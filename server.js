963779fbd3186b725b642865398af30706c893dd
Ana Grisham <anaunique@icloud.com>
2023-06-15 13:53:24 -0700

    moved file structure around

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) =>
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
 
 app.listen(PORT, () =>
