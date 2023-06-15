7bc7ab54ff3e4cc9783c296361ba44a6d3f90d31
Ana Grisham <anaunique@icloud.com>
2023-06-15 13:44:33 -0700

    added content to server file

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
 const express = require('express');
-// const
+const path = require('path');
+
+const api = require('./routes/index');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use('/api', api);
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
