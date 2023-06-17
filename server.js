a5b212a3f191dad2460bc06869331a126d490be6
Ana Grisham <anaunique@icloud.com>
2023-06-16 17:45:42 -0700

    added a delete method

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs')
-const notes = require('./db/db.json');
+let notes = require('./db/db.json');
 // const bodyParser = require('body-parser');
 const { readAndAppend } = require('./helpers/fsUtils');
 
@@ -45,35 +45,44 @@ app.post('/api/notes', (req, res) => {
       const newNote = {
         title,
         text,
-        note_id: uuid(),
+        id: uuid(),
       };
-      // readAndAppend(newNote, './db/db.json', res);
-  
-    // const reviewString = JSON.stringify(newNote);
-      fs.readFile('./db/db.json', 'utf8', (error, data) => {
-        console.log(JSON.parse(data));
-        let notes = JSON.parse(data);
+      // reading data from db.json file
+      // fs.readFile('./db/db.json', 'utf8', (error, data) => {
+      //   console.log(JSON.parse(data));
         notes.push(newNote)
       
-      
+      // writtin now data to the db.json file
       fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
         err
         ? console.error(err)
         : console.log(`Review for ${newNote.Title} has been written to JSON file`)
       });
-    })
+    // })
         console.log(req.body);
         res.status(201).json(notes);
       }else {
         res.status(500).json('Error posting note');
       }
-      // .getNotes()
-      // .then((notes) => {
-      //   return res.json(notes);
-      // })
       });  
     
+app.delete('/api/notes/:id', (req, res) => {
+  const newArray = [];
+    for (let note of notes) {
+     if (req.params.id !== note.id){
+      newArray.push(note);
+     } 
+    }
+    notes = newArray;
+    fs.writeFile('./db/db.json', JSON.stringify(newArray), (err) => {
+      err
+      ? console.error(err)
+      : console.log(`Note with ID ${req.params.id} has been deleted.`)
+      res.json("msg deleted");
+    });
+  })
 
+// calling on the server
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
